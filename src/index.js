158f6766e6d7ad09624cbdc0934924905d05aaa6
Teddy <brianrcdteodoro@gmail.com>
2019-12-07 17:01:02 +0800

    create a branch for season. state, app lifecycle, conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
-import faker from 'faker';
-import ApprovalCard from './ApprovalCard';
 
-const App = () => {
-	return (
-		<div className="ui containter comments">
-			<ApprovalCard>
-				<CommentDetail
-					author="Yema"
-					timeStamp="Today at 4:43PM"
-					comment="Kanina pa kita hinihintay"
-					avatar={faker.image.avatar()}
-				/>
-			</ApprovalCard>
-			<ApprovalCard>
-				<CommentDetail
-					author="Meg"
-					timeStamp="Today at 11:43AM"
-					comment="Panindigan"
-					avatar={faker.image.avatar()}
-				/>
-			</ApprovalCard>
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			latitude: null,
+			errorMessage: ''
+		};
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({ latitude: position.coords.latitude });
+			},
+			(err) => {
+				this.setState({ errorMessage: err.message });
+			}
+		);
+	}
+	render() {
+		if (this.state.errorMessage && !this.state.latitude) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
 
-			<ApprovalCard>
-				<CommentDetail
-					author="Ube"
-					timeStamp="Today at 6:43PM"
-					comment="Wait mo ko diyan Yema"
-					avatar={faker.image.avatar()}
-				/>
-			</ApprovalCard>
-		</div>
-	);
-};
+		if (!this.state.errorMessage && this.state.latitude) {
+			return <div>Latitude: {this.state.latitude}</div>;
+		}
+
+		return <div>Loading....</div>;
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
