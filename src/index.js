0fc93114173ad193692958bb5d914838ca2df4f8
Teddy <brianrcdteodoro@gmail.com>
2019-12-07 11:37:49 +0800

    props children

--- src/index.js
+++ src/index.js
@@ -2,28 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
 		<div className="ui containter comments">
-			<CommentDetail
-				author="Yema"
-				timeStamp="Today at 4:43PM"
-				comment="Kanina pa kita hinihintay"
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Meg"
-				timeStamp="Today at 11:43AM"
-				comment="Panindigan"
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Ube"
-				timeStamp="Today at 6:43PM"
-				comment="Wait mo ko diyan Yema"
-				avatar={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author="Yema"
+					timeStamp="Today at 4:43PM"
+					comment="Kanina pa kita hinihintay"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Meg"
+					timeStamp="Today at 11:43AM"
+					comment="Panindigan"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author="Ube"
+					timeStamp="Today at 6:43PM"
+					comment="Wait mo ko diyan Yema"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
