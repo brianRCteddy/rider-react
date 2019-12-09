f8d877f30f6e2c9ba4618063bfff2a5caab3c7f9
Teddy <brianrcdteodoro@gmail.com>
2019-12-09 13:41:24 +0800

    uncontrolled component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	state = {
+		term: ''
+	};
+	onInputChange(event) {
+		console.log(event.target.value);
+	}
+
+	render() {
+		return (
+			<div className="ui segment">
+				<form className="ui form">
+					<div className="field">
+						<label>Image Search: </label>
+						<input type="text" onChange={this.onInputChange} />
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
