950077c00bb5b8d5e297bbbba07cec7e8adb56f9
Teddy <brianrcdteodoro@gmail.com>
2019-12-09 17:03:01 +0800

    using promise

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios
+			.get('https://api.unsplash.com/search/photos', {
+				params: { query: term },
+				headers: {
+					Authorization: 'Client-ID df39419b935c69dd541f9c3b75b3d5940a05a3acc9e5605cbaede0e7b29fb807'
+				}
+			})
+			.then((response) => console.log(response.data.results), (err) => err);
 	}
 
 	render() {
