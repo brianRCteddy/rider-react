f3071fba93340d3b6cc6b2548d0961e69b82e5b7
Teddy <brianrcdteodoro@gmail.com>
2019-12-09 13:46:36 +0800

    controlled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	state = {
-		term: ''
+		term: 'Input keywords here'
 	};
-	onInputChange(event) {
-		console.log(event.target.value);
-	}
 
 	render() {
 		return (
@@ -14,7 +11,11 @@ class SearchBar extends Component {
 				<form className="ui form">
 					<div className="field">
 						<label>Image Search: </label>
-						<input type="text" onChange={this.onInputChange} />
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={(e) => this.setState({ term: e.target.value.toLocaleUpperCase() })}
+						/>
 					</div>
 				</form>
 			</div>
