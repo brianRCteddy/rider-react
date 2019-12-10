c2659f18f6107ed680e7049566f560d117bc4735
Teddy <brianrcdteodoro@gmail.com>
2019-12-10 15:48:41 +0800

    fixed inputChangeHandler

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	state = {
+		term: ''
+	};
+
+	inputChangeHandler = (e) => {
+		this.setState({ term: e.target.value });
+	};
+
+	submitFormHandler = (event) => {
+		event.preventDefault();
+
+		//Make sure we call
+		//callback from component
+	};
+
+	render() {
+		return (
+			<div className="search-bar ui segment">
+				<form className="ui form" action="submit" onSubmit={this}>
+					<div className="field">
+						<label>Video Search</label>
+						<input onChange={this.inputChangeHandler} type="text" value={this.state.term} />
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
